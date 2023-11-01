f44fbfd4747bd6b2b13078233066090dc5751306
Wander Pereira <wander.pereira@el.com.br>
2023-10-31 21:18:51 -0300

    Primeiro Commit do Projeto API REST com NodeJS

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import fastify from 'fastify'
+import { knex } from './connection'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running')
+  })
