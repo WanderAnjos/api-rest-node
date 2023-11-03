a3524b03bef1235367ae3b13bbd480d5eb799b31
Wander Pereira <wander.pereira@el.com.br>
2023-11-03 17:29:55 -0300

    Alters

--- src/connection.ts
+++ src/connection.ts
@@ -3,10 +3,10 @@ import { knex as setupKnex } from 'knex'
 import { env } from './env'
 
 export const config = {
-  client: env.DATABASE_CLIENTE,
-  connection: (env.DATABASE_CLIENTE === "sqlite" ? {
+  client: env.DATABASE_CLIENT,
+  connection: env.DATABASE_CLIENT === "sqlite" ? {
     filename: env.DATABASE_URL,
-  } : env.DATABASE_URL),
+  } : env.DATABASE_URL,
   useNullAsDefault: true
 }
 
